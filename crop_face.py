02ccda3600eb97d5dd134c0c4f678215ba28177c
vishakhsuresh98 <vishakhsuresh98@gmail.com>
2019-02-25 02:17:30 +0530

    face cropping

--- crop_face.py
+++ crop_face.py
@@ -1,18 +1,27 @@
 import dlib
-# import cv2
+import cv2
 from PIL import Image
 from skimage import io
 import matplotlib.pyplot as plt
 
 def detect_faces(image):
-
+    im = cv2.read(image)
+    (width, height) = im.shape
     # Create a face detector
     face_detector = dlib.get_frontal_face_detector()
 
     # Run detector and get bounding boxes of the faces on image.
     detected_faces = face_detector(image, 1)
-    face_frames = [(x.left(), round(0.4*x.top()),
-                    x.right(), x.bottom()) for x in detected_faces]
+    # face_frames = [(x.left(), round(0.4*x.top()),
+    #                x.right(), x.bottom()) for x in detected_faces]
+    face_frames = []
+    relaxation_factor = 0.25
+    for face in detected_faces:
+        relaxed_left = round((1-relaxation_factor)*face.left())
+        relaxed_top = round((1-relaxation_factor)*face.top())
+        relaxed_right = x.right() + round(relaxation_factor*(width-face.right()))
+        relaxed_bottom = x.bottom() + round(relaxation_factor*(height-face.bottom()))
+        face_frames.append((relaxed_left, relaxed_top, relaxed_right, relaxed_bottom))
     return face_frames
 
 # Load image
